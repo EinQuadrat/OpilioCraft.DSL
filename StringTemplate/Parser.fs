242a2ec1d2d578b0503c885588c2ff90f4ad402e
Carsten Schäfer <carsten.schaefer@mailbox.org>
2024-08-02 21:21:26 +0200

    Merged DSL projectes into one solution

---
+++ StringTemplate/Parser.fs
@@ -0,0 +1,26 @@
+﻿module OpilioCraft.DSL.StringTemplate.Parser
+
+open FParsec
+
+// narrow types
+type Parser<'t> = Parser<'t, unit>
+
+// lexical stuff
+let pOpeningBrace = pchar '{'
+let pClosingBrace = pchar '}'
+let isNormalChar = function | '{' | '|' | '}' -> false | _ -> true
+
+// elements
+let pPlainText =
+    manySatisfy isNormalChar |>> StringTemplateElement.PlainText
+
+let pPlaceholderParts =
+    sepBy1 (many1Satisfy isNormalChar) (pchar '|')
+
+let pPlaceholder =
+    pOpeningBrace >>. pPlaceholderParts .>> pClosingBrace
+    |>> fun args -> StringTemplateElement.Placeholder (args.Head, args.Tail)
+
+// string template
+let pStringTemplate : Parser<_> =
+    manyTill (pPlaceholder <|> pPlainText) eof
---
+++ StringTemplate/Program.fs
@@ -0,0 +1,14 @@
+﻿module OpilioCraft.DSL.StringTemplate.Main 
+// For more information see https://aka.ms/fsharp-console-apps
+
+let placeholderMap : PlaceholderMap =
+    Map.ofList
+        [
+            "name", (fun args -> args.Head)
+            "date", (fun _ -> "heute")
+        ]
+
+"Hallo {name|Carsten}! Wie war der Tag {date|MM.}?"
+|> Runtime.Parse
+|> Runtime.Eval placeholderMap (fun name -> failwith $"unknown placeholder: {name}")
+|> System.Console.WriteLine
